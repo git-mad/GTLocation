70a03992a13dce89e1af8a5ae11ccab2dbfacac8
kazilfr <kazilfr@gmail.com>
2024-02-15 17:05:49 -0500

    Add location permission and show on map

--- Map.js
+++ Map.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
+export default function Map() {
+  // Georgia Tech Coordinates
+  const init_reg = {
+    latitude: 33.7756,
+    longitude: -84.3963,
+    latitudeDelta: 0.029,
+    longitudeDelta: 0.015,
+  }
+  
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
+    
+    <SafeAreaView style={styles.container}>
+      <MapView style={styles.map} 
+      showsUserLocation = {true}
+      followsUserLocation = {true}
+      initialRegion={init_reg}
+      />
+    </SafeAreaView>
   );
+
+
+
+
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +38,4 @@ const styles = StyleSheet.create({
     height: '100%',
   },
 });
+
