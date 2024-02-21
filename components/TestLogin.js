0e989295cb91cae52a48f7bb8f80dd2807e0f7e2
Hai Dao <haixdao0923@gmail.com>
2024-02-21 17:19:18 -0500

    add firebase authentication

--- components/TestLogin.js
+++ components/TestLogin.js
@@ -4,15 +4,55 @@ import auth from '@react-native-firebase/auth';
 
 function TestLogin() {
   // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
+ 
     const signUpTest = () => {
         auth().createUserWithEmailAndPassword(email, password).then(() => {
             Alert.alert(`User Created with ${email} and ${password}`);
         }).catch((err)=> {
             console.log(err);
+            if (error.code == 'auth/invalid-email') {
+                console.log("Your email is not formatted correctly (You need @gmail.com)")
+            }
+            if (error.code == 'auth/weak-password') {
+                console.log("Your password need to be at least 6 characters")
+            }
         })
     }
+
+    const signIn = () => {
+        auth().signInWithEmailAndPassword(email, password).then(() => {
+            Alert.alert(`Signed in!`)
+        }).catch((err) => {
+            console.log(err);
+            if (error.code == 'auth/invalid-credential') {
+                console.log("Your username or password is incorrect")
+            }
+        })
+    }
+
+    const signOut = () => {
+        auth().signOut().then(() => {
+            Alert.alert(`Signed out!`)
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+      }, []);
+    
+    if (initializing) return null;
 
     return (
         <>
@@ -21,12 +61,25 @@ function TestLogin() {
             <Text>Password</Text>
             <TextInput editable onChangeText={text => {setPassword(text);}} value={password}/>
             <Text>Welcome {email}</Text>
-            <Button title={"Login"} onPress={() => {
-                console.log("Test");
-                Alert.alert("YAY");
+            <Button title={"Sign Up"} onPress={() => {
+                console.log("pressed sign up");
                 signUpTest();
-            }}>
+            }} disabled={email == '' || password == ''}>
             </Button>
+            <Button title="Sign in" onPress={() => {
+                console.log("Pressed sign in");
+                console.log(`User: ${user}`);
+                signIn();
+            }} disabled={email == '' || password == ''}></Button>
+            <Button title="Sign out" onPress={() => {
+                console.log("Pressed sign out");
+                signOut();
+            }} disabled={!user}></Button>
+            <Text>Test</Text>
+            {user ? <Text>{`Hi ${user.email}`}</Text> : <Text>Sad</Text>}
+            <Button title={"Display stats"} onPress={() => {
+                console.log(user);
+            }}></Button>
         </>
 
     );
