3b57574d8e57fb9117244e25a6a61417dc538dbf
SumukhP0 <spaspuleti3@gatech.edu>
2024-04-05 15:30:45 -0400

    Changed styling method for cards

--- components/InsightList.js
+++ components/InsightList.js
@@ -5,78 +5,19 @@ import React from "react";
 import { GlobalContext } from "../GlobalContext";
 
 // Seperates insights into first, second, and third lists
-const SeperateInsights = () => {
+export default InsightList = () => {
   const context = useContext(GlobalContext);
   const [insights, setInsights] = context.profile[3];
 
-  const [firstInsights, setFirstInsights] = useState([]);
-  const [secondInsights, setSecondInsights] = useState([]);
-  const [thirdInsights, setThirdInsights] = useState([]);
-
-  useEffect(() => {
-    // Checks and restricts iteration to not overflow on screen
-    let maxIndex = insights.leftCardViewInsights > 6 ? insights.length : 6;
-    console.log(maxIndex);
-
-    // Resets first, second, and third insight lists for expo re-rendering
-    setFirstInsights([]);
-    setSecondInsights([]);
-    setThirdInsights([]);
-
-    for (let i = 0; i < maxIndex; i++) {
-      switch (i) {
-        case 0:
-          setFirstInsights((prevState) => [...prevState, insights[i]]);
-          break;
-        case 1:
-          setFirstInsights((prevState) => [...prevState, insights[i]]);
-          break;
-        case 2:
-          setSecondInsights((prevState) => [...prevState, insights[i]]);
-          break;
-        case 3:
-          setSecondInsights((prevState) => [...prevState, insights[i]]);
-          break;
-        case 4:
-          setThirdInsights((prevState) => [...prevState, insights[i]]);
-          break;
-        case 5:
-          setThirdInsights((prevState) => [...prevState, insights[i]]);
-          break;
-      }
-    }
-  }, [insights]);
-
-  return { firstInsights, secondInsights, thirdInsights };
-};
-
-// Creates Card elements for insights
-export default InsightList = () => {
-  const { firstInsights, secondInsights, thirdInsights } = SeperateInsights();
-
-  //Renders Card elements for both left and right sides
+  //Renders Card elements
   return (
     <View styles={styles.containerStyle}>
-      {firstInsights.map((element, key) => (
-        <View style={styles.leftCardViewInsights} key={key}>
-          <Card style={styles.cardInsights}>
-            <Text style={styles.textInsights} key={element}>
-              {element}
-            </Text>
-          </Card>
-        </View>
-      ))}
-      {secondInsights.map((element, key) => (
-        <View style={styles.rightCardViewInsights} key={key}>
-          <Card style={styles.cardInsights}>
-            <Text style={styles.textInsights} key={element}>
-              {element}
-            </Text>
-          </Card>
-        </View>
-      ))}
-      {thirdInsights.map((element, key) => (
-        <View style={styles.leftCardViewInsights} key={key}>
+      {insights.map((element, index) => (
+        <View
+          style={styles.leftCardViewInsights}
+          justifyContent={index % 2 == 0 ? "start" : "flex-end"}
+          key={index}
+        >
           <Card style={styles.cardInsights}>
             <Text style={styles.textInsights} key={element}>
               {element}
@@ -99,12 +40,10 @@ const styles = StyleSheet.create({
   },
   leftCardViewInsights: {
     flexDirection: "row",
-    justifyContent: "start",
     padding: 15,
   },
   rightCardViewInsights: {
     flexDirection: "row",
-    justifyContent: "flex-end",
     padding: 15,
   },
 });
